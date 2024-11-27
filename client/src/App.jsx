833187f1ba202da0e6604d8442f3448094142365
nam <namish@tuta.io>
2024-11-27 21:23:12 +0530

    init tanstack and vite

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router"
+import { routeTree } from "./routeTree.gen"
+
+const router = createRouter({routeTree})
+function App() {
+  return <RouterProvider router={router}/>
+}
+
+export default App
