15fdfa3fc1cf0e2937c1ac989a7247d040fa0ea6
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2024-12-16 13:26:41 +0530

    fix: selenium options

--- server/twitter.py
+++ server/twitter.py
@@ -43,14 +43,14 @@ class TwitterScraper:
     def _renew_tokens(self) -> tuple[str, str]:
         # TODO: actually test this, twitter thinks i'm a bot for now so can't test it
         options = webdriver.ChromeOptions()
-        # options.add_argument("--headless=new")
-        # options.add_argument("--no-sandbox")
-        # options.add_argument("--disable-gpu")
-        # options.add_argument("--disable-dev-shm-usage")
-        # options.add_argument("--log-level=3")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
         if os.getenv("ENV") != "dev":
+            options.add_argument("--headless=new")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--log-level=3")
             driver = webdriver.Remote(
                 command_executor="http://selenium:4444/wd/hub", options=options
             )
