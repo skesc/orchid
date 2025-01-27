e8012b4ce0c9d33527ff09e576fe7f79a4ceec11
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2025-01-27 16:35:13 +0530

    fix: start.sh script for rembg, dont expose selenium to internet

--- server/rembg/start.sh
+++ server/rembg/start.sh
@@ -4,17 +4,19 @@ set -euo pipefail
 
 if [ -f .env ]; then
     export "$(cat .env | xargs)"
-else
-    echo "Error: .env file not found"
-    exit 1
 fi
+
+# set default model if not provided to u2net
+MODEL_NAME=${MODEL_NAME:-"u2net"}
 
 python3 -c "
 from rembg import new_session, remove
 from PIL import Image
 import numpy as np
+import os
 
-session = new_session(model_name='${MODEL_NAME}')
+model = os.getenv('MODEL_NAME')
+session = new_session(model_name=model)
 img = Image.new('RGB', (100, 100), color='red')
 remove(np.array(img), session=session)
 print(f'rembg preloaded successfully with model: {session.model_name}')
